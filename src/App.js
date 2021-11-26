c88b215a958ed319436ffc15a7b828d5f5ae51eb
daviszef <daviszefanya.dz@gmail.com>
2021-11-26 17:27:56 +0700

    update Meta

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 
 import './App.css';
 import { About, Header, Home, Project, Contact } from "./components"
-
+// import MetaTags from 'react-meta-tags';
+// import { DavisPic } from './assets'
 
 function App() {
   return (
     <div className="w-screen h-auto bg-mygrey">
+      {/* <MetaTags>
+        <title>Davis Zefanya</title>
+        <meta name="description" content="Get to know me here!!" />
+        <meta property="og:title" content="Davis Zefanya" />
+        <meta property="og:image" content={DavisPic} />
+      </MetaTags> */}
       <Header />
       <Home />
       <About />
       <Project />
       <Contact />
+      
     </div>
   );
 }
