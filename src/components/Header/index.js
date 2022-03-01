7ba6e747916b2667054287e8eb56f0c797675ce2
daviszef <daviszefanya.dz@gmail.com>
2022-03-01 11:59:54 +0700

    update resume

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,7 +13,7 @@ const Header = () => {
                     <div className="flex justify-between w-10/12 md:w-3/12 md:justify-between md:flex">
                         {/* <a href="#home" className="font-inter font-medium text-center cursor-pointer  hover:text-purple-600 visited:text-purple-600" onClick={()=>scroll.scrollToTop()} >Home</a> */}
                         <a href="#about" className="font-inter font-medium text-center cursor-pointer text-transparent bg-clip-text bg-gradient-to-r from-black to-black hover:from-indigo-500 hover:to-pink-500" onClick={()=>scroll.scrollTo(440)}>Work</a>
-                        <a href="https://drive.google.com/file/d/1ZtgLAfujBz9WOtAI_4xK-1eYxiOR6YI_/view?usp=sharing" className="font-inter font-medium text-center text-transparent bg-clip-text cursor-pointer bg-gradient-to-r from-black to-black hover:from-indigo-500 hover:to-pink-500" >Resume</a>
+                        <a href="https://drive.google.com/file/d/1GJAF8JSwftrq8nSqtyFeWGln3rSUoTvi/view?usp=sharing" className="font-inter font-medium text-center text-transparent bg-clip-text cursor-pointer bg-gradient-to-r from-black to-black hover:from-indigo-500 hover:to-pink-500" >Resume</a>
                         <a href="https://www.behance.net/daviszef" className="font-inter font-medium text-center text-transparent bg-clip-text cursor-pointer bg-gradient-to-r from-black to-black hover:from-indigo-500 hover:to-pink-500" >Behance</a>
                         <a href="#contact" className="font-inter font-medium text-center cursor-pointer text-transparent bg-clip-text bg-gradient-to-r from-black to-black hover:from-indigo-500 hover:to-pink-500" onClick={()=>scroll.scrollTo(1800)}>Contact</a>
                     </div>
